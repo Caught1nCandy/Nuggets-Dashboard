68158f3bc41bb2030e35e2ccb76f327e2ba313ad
KnikaKnack <knikanor@uccs.edu>
2026-02-24 14:38:29 -0800

    Add files via upload

---
+++ webdesign/admin/INFS Capstone/FEDEXHR.php
@@ -0,0 +1,34 @@
+<html> 
+<head> 
+
+<title> Dashboard Login</title>
+<link rel="stylesheet" href="Fedexlogin.css"> 
+ 
+</head> 
+
+<body> 
+
+<div class="header"> 
+  <img src="fimg/FLogo.png" width="100" height="50"> 
+</div>
+
+
+
+<h3 style="color:white; font-size:50px;"> Dashboard Login </h3>
+
+<form class="login" action="FHandler.php" method="post">    
+  <input type="text" name="name" placeholder="Username"><br>
+  <input type="password" name="pswd" placeholder="Password"><br>
+  <input type="hidden" name="ip"> 
+  <input type="submit" value="Enter">
+</form>
+
+
+<footer>
+  
+</footer>
+
+
+</body> 
+
+</html> 
