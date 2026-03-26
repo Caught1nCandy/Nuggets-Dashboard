8bfeb617c345f30118a99109feb68709e77b42f7
Caught1nCandy <Brady.Walpole@protonmail.com>
2026-03-26 17:01:03 -0600

    Update events.php

--- webdesign/admin/INFS Capstone/events.php
+++ webdesign/admin/INFS Capstone/events.php
@@ -423,10 +423,11 @@ foreach ($anniversaryRows as $r) {
 </head>
 <body>
 <?php include __DIR__ . '/impersonation_banner.php'; ?>
+<?php $activePage = 'events'; include __DIR__ . '/navbar.php'; ?>
   <div class="site-header">
     <div class="orange-bar"></div>
     <span class="page-title">Events</span>
-<?php $activePage = 'events'; include __DIR__ . '/navbar.php'; ?>
+
 
   <div class="cal-wrapper">
 
